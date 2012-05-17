040210e61c51a022b12cddec8fd3f0e53e62ae09
Jakob Gruber <jakob.gruber@gmail.com>
2012-05-17 20:02:03 +0200

    Highlight failed test results

--- test.sh
+++ test.sh
@@ -2,6 +2,11 @@
 
 for comp in w psh c f s; do
     for file in $(find . -name "$comp.test[0-9]*.dl"); do
-        dlv $comp.dl $file component.tester.dl -N=60 | grep '{'
+        op=$(dlv $comp.dl $file component.tester.dl -N=60)
+        if [ -z $1 ]; then
+           echo "$op" | grep '{' | grep --color=auto '[A-Z]\+_[cp]([^)]*)'
+        else
+           echo "$op"
+       fi
     done
 done
