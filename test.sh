60a9217f6d1fc2e15cfa2105ad076c07f2d7a630
Jakob Gruber <jakob.gruber@gmail.com>
2012-05-18 20:33:24 +0200

    Prepare for connect test cases

--- test.sh
+++ test.sh
@@ -10,3 +10,12 @@ for comp in w psh c f s; do
        fi
     done
 done
+
+for file in $(find . -name "connect.test[0-9]*.dl"); do
+    op=$(dlv connect.dl $file w.dl psh.dl c.dl f.dl s.dl connect.tester.dl -N=60)
+    if [ -z $1 ]; then
+       echo "$op" | grep '{' | grep --color=auto '[A-Z]\+_[cp]([^)]*)'
+    else
+       echo "$op"
+   fi
+done
